0d20d3f9b852211363b3608adfc52865dc9bef66
Italo Teles <italop.teles@gmail.com>
2023-02-09 17:20:24 -0300

    template do painel principal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { LoginComponent } from './components/login/login.component';
 import { AuthenticationService } from './services/authentication.service';
 import { PainelPrincipalComponent } from './components/painel-principal/painel-principal.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/painel-principal/header/header.component';
+import { FooterComponent } from './components/painel-principal/footer/footer.component';
+import { ConteudoComponent } from './components/painel-principal/conteudo/conteudo.component';
 
 
 @NgModule({
@@ -15,7 +18,10 @@ import { RegisterComponent } from './components/register/register.component';
     AppComponent,
     LoginComponent,
     PainelPrincipalComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent,
+    ConteudoComponent
   ],
   imports: [
     BrowserModule,
